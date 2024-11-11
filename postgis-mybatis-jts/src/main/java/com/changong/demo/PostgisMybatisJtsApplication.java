8b7c2412fa64dc86c68545fa00cc10d3758fa897
张赛超 <1228310398@qq.com>
2024-11-11 09:35:51 +0800

    1.调整postgis-mybatis-jts模块；

---
+++ postgis-mybatis-jts/src/main/java/com/changong/demo/PostgisMybatisJtsApplication.java
@@ -0,0 +1,16 @@
+package com.changong.demo;
+
+/**
+ * <p>
+ *  TODO
+ * </p>
+ *
+ * @author 张赛超
+ * @version 1.0.0
+ * @since 2024/11/11
+ **/
+public class PostgisMybatisJtsApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
