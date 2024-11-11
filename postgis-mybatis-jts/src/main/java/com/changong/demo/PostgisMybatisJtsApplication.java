c553aa7dd966a56020e123cf79c4b847be964a89
张赛超 <1228310398@qq.com>
2024-11-11 19:41:02 +0800

    1.临时提交；

--- postgis-mybatis-jts/src/main/java/com/changong/demo/PostgisMybatisJtsApplication.java
+++ postgis-mybatis-jts/src/main/java/com/changong/demo/PostgisMybatisJtsApplication.java
@@ -2,13 +2,14 @@ package com.changong.demo;
 
 /**
  * <p>
- *  TODO
+ *  PostgisMybatisJts 启动类
  * </p>
  *
  * @author 张赛超
  * @version 1.0.0
  * @since 2024/11/11
  **/
+@SpringBootApplication
 public class PostgisMybatisJtsApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
